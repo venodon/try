f071f642439cd142b5b005d50a2db2519a4c135c
venodon <venodon@gmail.com>
2022-11-21 19:14:02 +0300

    добавление фильтра по активности

--- fuel/app/classes/presenter/menu.php
+++ fuel/app/classes/presenter/menu.php
@@ -7,6 +7,6 @@ class Presenter_Menu extends Presenter
     public function view()
     {
         $this->title = 'Меню презентер';
-        $this->posts = Model_Post::find('all');
+        $this->posts = Model_Post::query()->where('status', '=', Model_Post::STATUS_ACTIVE)->get();
     }
 }
